c4031a6dde4b7a0f7c3ec977715ff915e7ff0878
Juan <jbellevue83022@gmail.com>
2024-12-18 03:23:29 -0600

    Added JS to dynamcally create a book when add book is pressed

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const modal = document.querySelector('.modal')
 const openModal = document.querySelector('.add-book')
 const closeModal = document.querySelector('.x')
 const bookForm = document.querySelector('.add-book-form')
+const main = document.querySelector('.books')
+
 
 openModal.addEventListener('click', () => {modal.showModal();})
 closeModal.addEventListener('click', () => {modal.close();})
@@ -13,7 +15,39 @@ const author = document.getElementById("author").value;
 const pages = document.getElementById("pages").value;
 const haveRead = document.getElementById("haveRead").checked;
 
-const myLibrary = [];
+const myLibrary = [ { title: "1984", author: "George Orwell", pages: 328, haveRead: false },
+    { title: "To Kill a Mockingbird", author: "Harper Lee", pages: 281, haveRead: false },
+    { title: "The Great Gatsby", author: "F. Scott Fitzgerald", pages: 180, haveRead: false }
+];
+
+bookForm.addEventListener('submit', (event) => {
+    event.preventDefault;
+
+
+    // Get input values dynamically
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const haveRead = document.getElementById("haveRead").checked;
+
+    addBookToLibrary(title, author, pages, haveRead);
+
+    bookForm.reset(); // Reset the form after submission
+    
+    modal.close(); // Close the modal
+});
+ 
+function renderBook(title, author, pages, haveRead){
+    bookObj = document.createElement("div");
+    bookObj.innerHTML = `<p>Title: ${title}</p>
+    <p>Author: ${author}</p>
+    <p>pages: ${pages}</p>
+    <p>Read?</p>`
+    bookObj.className = "book";
+    main.appendChild(bookObj);
+   
+
+}
 
 function Book(title, author, pages, haveRead){
     this.title = title,
@@ -24,6 +58,9 @@ function Book(title, author, pages, haveRead){
 }
 
 function addBookToLibrary(title, author, pages, haveRead) {
-  
+    const newBook = new Book(title, author, pages, haveRead);
+    myLibrary.push(newBook)
+    renderBook(title, author, pages, haveRead);
+    console.log("pushing book!")
 }
 
